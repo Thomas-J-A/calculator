ef1d759a84f33b158eb6a2e127de1a8fc498d086
Thomas <tomaspbury@icloud.com>
2021-02-05 20:12:05 +0000

    Add keyboard support

--- main.js
+++ main.js
@@ -5,35 +5,46 @@ let operator, operand1;
 // handlers for all number buttons
 const numbers = Array.from(document.getElementsByClassName('number'));
 numbers.forEach(number => {
-    number.addEventListener('click', e => {
-        displayValue += e.target.textContent;
-        updateDisplay();
-    });
+    number.onclick = function(e, x) { // x used when no access to event object
+        if (x) {  
+            displayValue += x;
+            updateDisplay();
+        } else {
+            displayValue += e.target.textContent;
+            updateDisplay();
+        }
+    };
 });
 
 // handlers for all operator buttons
 const operators = Array.from(document.getElementsByClassName('operator'));
 operators.forEach(op => {
-    op.addEventListener('click', e => {
+    op.onclick = function(e, x) {  // x used when no access to event object
         if (operator) {
             displayValue = operate(operator, operand1, displayValue);
             updateDisplay();
         }
-        operator = e.target.textContent;
-        operand1 = displayValue;
-        displayValue = '';
-    });
+        if (x) {
+            operator = x;
+            operand1 = displayValue;
+            displayValue = '';
+        } else {
+            operator = e.target.textContent;
+            operand1 = displayValue;
+            displayValue = '';
+        }
+    };
 });
 
 // handler for equals button
 const equalsBtn = document.getElementById('equals');
-equalsBtn.addEventListener('click', () => {
+equalsBtn.onclick = function() {
     if (displayValue === '0' && operator === '/') {  // division by 0
         displayValue = 'Divison by zero...';
         updateDisplay();
-    } else if (!operator && !operand1) {  // press equals before operator
+    } else if (!operator && !operand1) {  // pressed equals before operator
         console.error('pressed equals before operator');
-    } else if (!displayValue) {  // press operator then equals
+    } else if (!displayValue) {  // pressed operator then equals
         if (operand1 === '0') {
             displayValue = 'Divison by zero...';
             updateDisplay();
@@ -49,33 +60,71 @@ equalsBtn.addEventListener('click', () => {
         operand1 = '';
         updateDisplay();
     }
-});
+};
 
 // handler for all clear button (AC) 
 const acBtn = document.getElementById('ac');
-acBtn.addEventListener('click', () => {
+acBtn.onclick = function() {
     displayValue = '';
     operator = '';
     operand1 = '';
     updateDisplay();
-});
+};
 
 // handler for clear button (C)
 const cBtn = document.getElementById('c');
-cBtn.addEventListener('click', () => {
+cBtn.onclick = function() {
     displayValue = '';
     updateDisplay();
-});
+};
 
 // handler for floating point button (.) 
 const floatBtn = document.getElementById('decimal');
-floatBtn.addEventListener('click', () => {
+floatBtn.onclick = function() {
     if (/\./.test(displayValue) == true) {
         console.error('more than one decimal point');
     } else {
         displayValue += '.';
         updateDisplay();
     }
+};
+
+// keyboard support
+window.addEventListener('keydown', e => {
+    e.preventDefault();  // prevent default behaviour of '/' key in browser
+    let key = e.key;
+    switch (true) {
+        case /[0-9]/.test(key):            // numerical key pressed
+            let num;
+            for (let i = 0; i < numbers.length; i++) {    // get DOM object
+                if (numbers[i].textContent === key) {
+                    num = numbers[i];
+                } 
+            }
+            num.onclick(e, num.textContent);  // programmatically run handler
+            break;                         
+        case /[\+\-\*\/]/ .test(key):      // operator key pressed
+            let op;
+            for (let i = 0; i < operators.length; i++) {
+                if (operators[i].textContent === key) {
+                    op = operators[i];
+                }
+            }
+            op.onclick(e, op.textContent);
+            break;                            
+        case /\./.test(key):               // decimal point key pressed
+            floatBtn.onclick();
+            break;
+        case /Delete/.test(key):           // delete key pressed (AC)
+            acBtn.onclick();
+            break;
+        case /Backspace/.test(key):        // backspace key pressed (C)
+            cBtn.onclick();
+            break;
+        case /=/.test(key):                // equals key pressed
+            equalsBtn.onclick();
+            break;      
+    }    
 });
 
 
@@ -83,20 +132,6 @@ floatBtn.addEventListener('click', () => {
 function updateDisplay() {
     screen.textContent = displayValue;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function add(x, y) {
@@ -114,6 +149,7 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
 
 function operate(operator, x, y) {
     switch (operator) {
